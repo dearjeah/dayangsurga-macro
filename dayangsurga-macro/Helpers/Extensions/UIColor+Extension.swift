c829f2b6569cfda5ef02f4a199e67c31012ac19c
Delvina J <j.delvina@gmail.com>
2021-10-25 20:47:54 +0700

    add color, swiftgen, and RoundedButton

--- dayangsurga-macro/Helpers/Extensions/UIColor+Extension.swift
+++ dayangsurga-macro/Helpers/Extensions/UIColor+Extension.swift
@@ -2,9 +2,7 @@
 //  UIColor+Extension.swift
 //  dayangsurga-macro
 //
-//  Created by Delvina Janice on 22/10/21.
-//
-
+//  Created by Dayang Surga Team.
 //  swiftlint:disable all
 
 import UIKit
@@ -13,8 +11,16 @@ import UIKit
 
 extension UIColor {
 
+    static let primaryBG = UIColor.color(named: "primaryBG")
+    static let primaryBlue = UIColor.color(named: "primaryBlue")
+    static let primaryDisable = UIColor.color(named: "primaryDisable")
+    static let primaryRed = UIColor.color(named: "primaryRed")
+    static let primaryWhite = UIColor.color(named: "primaryWhite")
+    static let primaryYellow = UIColor.color(named: "primaryYellow")
+
     private static func color(named: String) -> UIColor {
         return UIColor(named: named)!
     }
 
 }
+
