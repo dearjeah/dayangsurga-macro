c5b86769a9be23da3038b38462214fb8ec100984
Olip <oliviadwisusanti@gmail.com>
2021-10-26 22:05:36 +0700

    add crud func and preload

---
+++ dayangsurga-macro/Core/Repository/ExperienceSuggestionRepository.swift
@@ -0,0 +1,92 @@
+//
+//  ExperienceSuggestionRepository.swift
+//  dayangsurga-macro
+//
+//  Created by Olivia Dwi  Susanti on 24/10/21.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class ExperienceSuggestionRepository{
+    
+    static let shared = ExperienceSuggestionRepository()
+    let entityName = Experience_Suggestion.self.description()
+    let context = CoreDataManager.sharedManager.persistentContainer.viewContext
+    
+    // create data
+    func createExpSuggestion(exp_suggest_id: Int,
+                             exp_id: Int,
+                             jobDescSuggest: String){
+        do {
+            let experienceSuggestion = Experience_Suggestion(context: context)
+            experienceSuggestion.exp_suggest_id = Int32(exp_suggest_id)
+            experienceSuggestion.exp_id = Int32(exp_id)
+            experienceSuggestion.jobDescSuggest = jobDescSuggest
+            
+            // relation (one-to-one) experience to exp suggestion
+            if let experienceSuggestions = ExperienceRepository.shared.getExperienceById(experienceId: exp_id){
+                experienceSuggestion.experience = experienceSuggestions
+            }
+            
+            try context.save()
+        }
+        catch let error as NSError {
+            print(error)
+        }
+    }
+    
+    // retrieve
+    func getAllExpSuggestion() -> [Experience_Suggestion]? {
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: entityName)
+        do {
+            let item = try context.fetch(fetchRequest) as? [Experience_Suggestion]
+            return item
+        } catch let error as NSError {
+            print(error)
+        }
+        return []
+    }
+    
+    func getExpSuggestionById(exp_suggest_id: Int) -> Experience_Suggestion? {
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: entityName)
+        fetchRequest.predicate = NSPredicate(format: "exp_suggest_id == %@", exp_suggest_id as CVarArg)
+        do {
+            let item = try context.fetch(fetchRequest) as? [Experience_Suggestion]
+            return item?.first
+        } catch let error as NSError {
+            print(error)
+        }
+        return nil
+    }
+    
+    // func updates
+    func updateExpSuggestion(exp_suggest_id: Int,
+                             exp_id: Int,
+                             newwJobDescSuggest: String) {
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: entityName)
+        fetchRequest.predicate = NSPredicate(format: "exp_suggest_id == %@", exp_suggest_id as CVarArg)
+        do {
+            let item = try context.fetch(fetchRequest) as? [Experience_Suggestion]
+            let experienceSuggest = item?.first
+            experienceSuggest?.jobDescSuggest = newwJobDescSuggest
+            
+            try context.save()
+        } catch let error as NSError {
+            print(error)
+        }
+        
+    }
+    
+    // func delete
+    func deleteExpSuggestion(data: Experience_Suggestion) {
+        do {
+            context.delete(data)
+            try context.save()
+            
+        } catch let error as NSError {
+            print(error)
+        }
+    }
+}
---
+++ dayangsurga-macro/Core/Repository/ResumeContentRepository.swift
@@ -0,0 +1,102 @@
+//
+//  ResumeContentRepository.swift
+//  dayangsurga-macro
+//
+//  Created by Olivia Dwi  Susanti on 24/10/21.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class ResumeContentRepository{
+    
+    static let shared = ResumeContentRepository()
+    let entityName = Resume_Content.self.description()
+    let context = CoreDataManager.sharedManager.persistentContainer.viewContext
+    
+    // create data
+    func createResumeContent(resume_id: Int,
+                             exp_id: Int,
+                             edu_id: Int,
+                             accom_id: Int,
+                             skill_id: Int){
+        do {
+            // relation ke resume
+            if let getResume = UserResumeRepository.shared.getUserResumeById(resume_id: resume_id){
+                let resumeContent = Resume_Content(context: context)
+                resumeContent.resume_id = Int32(resume_id)
+                
+                // relation ke experience
+                if let getExp = ExperienceRepository.shared.getExperienceById(experienceId: exp_id){
+                    getExp.exp_id = Int32(exp_id)
+                    getExp.addToResumeContent(resumeContent)
+                }
+                
+                // relation dari template
+                getResume.addToResumeContent(resumeContent)
+                try context.save()
+            }
+        }
+        catch let error as NSError {
+            print(error)
+        }
+    }
+    
+    // retrieve
+    func getAllResumeContent() -> [Resume_Content]? {
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: entityName)
+        do {
+            let item = try context.fetch(fetchRequest) as? [Resume_Content]
+            return item
+        } catch let error as NSError {
+            print(error)
+        }
+        return []
+    }
+    
+    func getResumeContentById(resume_id: Int) -> Resume_Content? {
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: entityName)
+        fetchRequest.predicate = NSPredicate(format: "resume_id == %@", resume_id as CVarArg)
+        do {
+            let item = try context.fetch(fetchRequest) as? [Resume_Content]
+            return item?.first
+        } catch let error as NSError {
+            print(error)
+        }
+        return nil
+    }
+    
+    // func updates
+    func updateResumeContent(resume_id: Int,
+                             newExp_id: Int,
+                             newEdu_id: Int,
+                             newAccom_id: Int,
+                             newSkill_id: Int) {
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: entityName)
+        fetchRequest.predicate = NSPredicate(format: "resume_id == %@", resume_id as CVarArg)
+        do {
+            let item = try context.fetch(fetchRequest) as? [Resume_Content]
+            let newResumeContent = item?.first
+            newResumeContent?.exp_id = Int32(newExp_id)
+            newResumeContent?.edu_id = Int32(newEdu_id)
+            newResumeContent?.accom_id = Int32(newAccom_id)
+            newResumeContent?.skill_id = Int32(newSkill_id)
+            
+            try context.save()
+        } catch let error as NSError {
+            print(error)
+        }
+    }
+    
+    // func delete
+    func deleteUser(data: Resume_Content) {
+        do {
+            context.delete(data)
+            try context.save()
+            
+        } catch let error as NSError {
+            print(error)
+        }
+    }
+}
