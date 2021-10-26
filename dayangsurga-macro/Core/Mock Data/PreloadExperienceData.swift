164a8952b0f9f235cff76e4f12b8bf1a491b6e18
Olip <oliviadwisusanti@gmail.com>
2021-10-26 21:00:55 +0700

    Update preload data
    For User and Experience

--- dayangsurga-macro/Core/Mock Data/PreloadExperienceData.swift
+++ dayangsurga-macro/Core/Mock Data/PreloadExperienceData.swift
@@ -14,15 +14,15 @@ class PreloadExperienceData {
     func preloadExpSuggetsion(){
         ExperienceSuggestionRepository.shared.createExpSuggestion(exp_suggest_id: 1,
                                                                   exp_id: 0,
-                                                                  jobDescSuggest: "")
+                                                                  jobDescSuggest: "Describe what you have done before that matches with the job qualifications, make sure you use action verbs.")
     }
     
     // preload exp placeholder
     func preloadExpPh() {
         ExperiencePlaceholderRepository.shared.createExpPh(exp_ph_id: 1,
                                                            exp_id: 0,
-                                                           jobTitle_Ph: "",
-                                                           jobDesc_ph: "",
-                                                           companyName_ph: "")
+                                                           jobTitle_Ph: "e.g. Apple Inc",
+                                                           jobDesc_ph: "e.g. Project Manager",
+                                                           companyName_ph: "Lead a new CLOAD application project with a team of 5 people focusing on ATS-friendly resume.")
     }
 }
--- dayangsurga-macro/Core/Mock Data/PreloadUserData.swift
+++ dayangsurga-macro/Core/Mock Data/PreloadUserData.swift
@@ -14,17 +14,17 @@ class PreloadUserData {
     func preloadUserSuggetsion(){
         UserSuggestionRepository.shared.createUserSuggestion(user_suggestion_id: 1,
                                                              user_id: 0,
-                                                             summarySuggest: "")
+                                                             summarySuggest: "Tell us about who you are and what you do that fits the job you're applying for, make sure you use action verbs.")
     }
     
     // preload user placeholder
     func preloadUserPh() {
         UserPlaceholderRepository.shared.createUserPh(user_ph_id: 1,
                                                       user_id: 0,
-                                                      name_ph: "",
-                                                      phoneNumber_ph: "",
-                                                      email_ph: "",
-                                                      address_ph: "",
-                                                      summary_ph: "")
+                                                      name_ph: "e.g. Olipia Dayangz",
+                                                      phoneNumber_ph: "e.g. 08123456789",
+                                                      email_ph: "e.g. Olipia@gmail.com",
+                                                      address_ph: "e.g. Jakarta, Indonesia",
+                                                      summary_ph: "e.g. Highly capable Product Manager with 1+ years experience in technology companies, seeking to apply strategic planning to grow revenue and market share. ")
     }
 }
