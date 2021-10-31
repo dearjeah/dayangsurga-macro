d93cd97450c08b29029cb4ea80da7321bfdc3df2
Audrey Aurelia Chang <audreyaureliachang@gmail.com>
2021-10-31 16:57:41 +0700

    Added Preview Resume page display
    Added auto layout on components in preview resume page
    Added resources needed for preview resume page

--- dayangsurga-macro/Features/CreateResume/GenerateResumePage/GenerateResumeViewController.swift
+++ dayangsurga-macro/Features/CreateResume/GenerateResumePage/GenerateResumeViewController.swift
@@ -9,13 +9,42 @@ import UIKit
 
 class GenerateResumeController: MVVMViewController<GenerateResumeViewModel> {
 
+    @IBOutlet weak var resumePreviewImage: UIImageView!
+    @IBOutlet weak var resumeName: UILabel!
+    @IBOutlet weak var resumeDate: UILabel!
+    @IBOutlet weak var editResumeNameButton: UIButton!
+    @IBOutlet weak var previewResumeButton: UIButton!
+    @IBOutlet weak var exportResumeButton: UIButton!
+    @IBOutlet weak var finishCreateResume: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        exportResumeButton.dsLongFilledPrimaryButton(withImage: false, text: " Export Resume")
+        exportResumeButton.tintColor = .white
+        finishCreateResume.dsLongUnfilledButton(isDelete: false, text: "Finish")
         // Do any additional setup after loading the view.
     }
+  
     
-
+    @IBAction func previewDidTap(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "PreviewResumeViewController", bundle: nil)
+        let vc = storyboard.instantiateViewController(identifier: "goToPreviewResume") as! PreviewResumeViewController
+        self.navigationController?.navigationBar.prefersLargeTitles = false
+        self.navigationItem.titleView?.tintColor = .white
+        self.tabBarController?.tabBar.isHidden = true
+        self.navigationController?.pushViewController(vc, animated: true)
+       
+    }
+    
+    
+    @IBAction func finishDidTap(_ sender: Any) {
+      
+        
+    }
+    
+    @IBAction func exportDidTap(_ sender: Any) {
+    }
+    
     /*
     // MARK: - Navigation
 
