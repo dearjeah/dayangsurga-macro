0b884ee31822d7cbf7fca7cfaa3eae9cb236ddae
Delvina J <j.delvina@gmail.com>
2021-11-02 10:37:41 +0700

    update accomp cell

---
+++ dayangsurga-macro/Features/CreateResume/StepByStepGuide/AccomplishmentTableCell/AccomplishmentTableCell.swift
@@ -0,0 +1,59 @@
+//
+//  AccomplishmentTableCell.swift
+//  dayangsurga-macro
+//
+//  Created by Audrey Aurelia Chang on 25/10/21.
+//
+
+import UIKit
+
+class AccomplishmentTableCell: UITableViewCell {
+
+    @IBOutlet weak var awardName: UILabel!
+    @IBOutlet weak var awardDate: UILabel!
+    @IBOutlet weak var awardIssuer: UILabel!
+    @IBOutlet weak var selectionButton: UIButton!
+    @IBOutlet weak var editAwardButton: UIButton!
+    @IBOutlet weak var shadowView: DesignableButton!
+    static let identifier = "AccomplishmentTableCell"
+    var selectionStatus = false
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        shadowView.layer.borderWidth = 1
+        shadowView.layer.cornerRadius = 17
+        selectionButton.setImage(UIImage(named: "icRoundSelectionFilled"), for: .normal)
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        self.layer.borderColor = UIColor.primaryBlue.cgColor
+        // Configure the view for the selected state
+    }
+    
+    override func layoutSubviews() {
+        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 0, left: 0, bottom: 12, right: 0))
+    }
+    
+    @IBAction func selectAccomplishment(_ sender: Any) {
+        if selectionStatus == false{
+            selectionStatus = true
+            selectionButton.setImage(UIImage(named: "icRoundSelectionFilled"), for: .normal)
+            shadowView.layer.borderWidth = 1
+            shadowView.layer.borderColor = UIColor.primaryBlue.cgColor
+        }else{
+            selectionStatus = false
+            selectionButton.setImage(UIImage(named: "icRoundSelectionNoFill"), for: .normal)
+            shadowView.layer.borderColor = UIColor.clear.cgColor
+        }
+    }
+    
+    @IBAction func editAccomplishment(_ sender: Any) {
+    }
+    
+    
+    static func nib() -> UINib {
+        return UINib(nibName: "AccomplishmentTableCell", bundle: nil)
+    }
+}
