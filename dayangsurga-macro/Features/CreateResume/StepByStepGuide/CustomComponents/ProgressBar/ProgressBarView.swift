c18497ebe52136cad786ca7e2c0cb4562b80eaf1
Delvina J <j.delvina@gmail.com>
2021-11-03 11:10:52 +0700

    progress bar update

--- dayangsurga-macro/Features/CreateResume/StepByStepGuide/CustomComponents/ProgressBar/ProgressBarView.swift
+++ dayangsurga-macro/Features/CreateResume/StepByStepGuide/CustomComponents/ProgressBar/ProgressBarView.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ProgressBarView: UIView {
-
+    
     @IBOutlet weak var personalInformationButton: UIButton!
     @IBOutlet weak var personalInformationButtonImage: UIImageView!
     @IBOutlet weak var educationButton: UIButton!
@@ -20,12 +20,51 @@ class ProgressBarView: UIView {
     @IBOutlet weak var accomplishmentButtonImage: UIImageView!
     @IBOutlet weak var accomplishmentButton: UIButton!
     
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        //tag: 1-5 1: personal info, 2: education 3: exp, 4: skills, 5: accmomp
+        switch sender.tag {
+        case 1:
+            print("personal info")
+        case 2:
+            print("edu")
+        case 3:
+            print("exp")
+        case 4:
+            print("skills")
+        case 5:
+            print("accomp")
+        default:
+            print("nothing")
+        }
+        
+        
     }
-    */
-
+    
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initWithNib()
+    }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initWithNib()
+    }
+    fileprivate func initWithNib() {
+        guard let view = loadViewFromNib(nibName: "ProgressBarView") else {return}
+        view.frame = self.bounds
+        self.addSubview(view)
+    }
+    
+    convenience init() {
+        self.init()
+    }
+    
+    func loadViewFromNib(nibName: String) -> UIView? {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: nibName, bundle: bundle)
+        
+        return nib.instantiate(withOwner: self, options: nil).first as? UIView
+    }
+    
 }
