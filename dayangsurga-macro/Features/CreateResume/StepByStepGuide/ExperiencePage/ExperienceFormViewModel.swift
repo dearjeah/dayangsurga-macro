f7d7d034dcf924d0ff34d693cdc9bf4820f2fdd5
Olip <oliviadwisusanti@gmail.com>
2021-11-07 17:58:35 +0700

    add function into viewmodel

--- dayangsurga-macro/Features/CreateResume/StepByStepGuide/ExperiencePage/ExperienceFormViewModel.swift
+++ dayangsurga-macro/Features/CreateResume/StepByStepGuide/ExperiencePage/ExperienceFormViewModel.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import UIKit
+
+class ExperienceFormViewModel: NSObject{
+    
+    let expSuggestionRepo = ExperienceSuggestionRepository.shared
+    let expPhRepo = ExperiencePlaceholderRepository.shared
+    let expRepo = ExperienceRepository.shared
+    
+    
+    // for experience suggestion + ph
+    func getExpSuggestion() -> Experience_Suggestion?{
+        return expSuggestionRepo.getExpSuggestionById(exp_suggest_id: 1)
+    }
+    
+    func getExpPh() -> Experience_Placeholder?{
+        return expPhRepo.getExpPhById(id: 1)
+    }
+    
+    func getAllExpPh() -> [Experience_Placeholder]?{
+        return expPhRepo.getAllExpPh()
+    }
+
+}
