55b20771e5c9f9ab1eee245ecb96878a0d6b784f
Olip <oliviadwisusanti@gmail.com>
2021-11-08 15:38:36 +0700

    change button to datepicker

--- dayangsurga-macro/Features/GlobalCustomComponent/LabelWithStartEndDate/LabelWithStartEndDate.swift
+++ dayangsurga-macro/Features/GlobalCustomComponent/LabelWithStartEndDate/LabelWithStartEndDate.swift
@@ -13,16 +13,16 @@ class LabelWithStartEndDate: UIView {
     @IBOutlet weak var startDatePicker: UIDatePicker!
     @IBOutlet weak var endDatePicker: UIDatePicker!
     //    @IBOutlet weak var startDateButton: UIButton!
-    //    @IBOutlet weak var endDateButton: UIButton!
-    
+//    @IBOutlet weak var endDateButton: UIButton!
+//
 //    @IBAction func startDatePressed(_ sender: UIButton) {
 //    }
 //
 //    @IBAction func endDatePressed(_ sender: UIButton) {
 //    }
-    @IBAction func startDatePickerPress(_ sender: Any) {
+    @IBAction func startDateWasPress(_ sender: Any) {
     }
-    @IBAction func endDatePicekrPress(_ sender: Any) {
+    @IBAction func endDateWasPress(_ sender: Any) {
     }
     
     override init(frame: CGRect) {
@@ -38,8 +38,8 @@ class LabelWithStartEndDate: UIView {
     convenience init(title: String, startDateData: String?, endDateData: String?) {
         self.init()
         titleLabel.text = title
-        startDateButton.titleLabel?.text = startDateData ?? "July 2020"
-        endDateButton.titleLabel?.text = endDateData ?? "June 2024"
+//        startDateButton.titleLabel?.text = startDateData ?? "July 2020"
+//        endDateButton.titleLabel?.text = endDateData ?? "June 2024"
     }
     
     fileprivate func initWithNib() {
