8d850dc62c8d1616ffc8dbe6b8f78935ba8c116d
Audrey Aurelia Chang <audreyaureliachang@gmail.com>
2021-11-09 21:03:07 +0700

    Added ExpertInfoCell XIB
    Created XIB file for expert info cell
    Finished adding visual elements for the XIB

---
+++ dayangsurga-macro/Features/AskExpert/ExpertInfoPage/CustomComponents/ExpertInfoCell.swift
@@ -0,0 +1,23 @@
+//
+//  ExpertInfoCell.swift
+//  dayangsurga-macro
+//
+//  Created by Audrey Aurelia Chang on 09/11/21.
+//
+
+import UIKit
+
+class ExpertInfoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
