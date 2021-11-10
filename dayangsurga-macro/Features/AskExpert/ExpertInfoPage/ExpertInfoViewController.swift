5421132c6c8c3fd84601baffcd74f153621e8944
Audrey Aurelia Chang <audreyaureliachang@gmail.com>
2021-11-10 20:57:21 +0700

    Added new assets and visual components
    Added visual components for ExpertInfoPage
    Added assets for ExpertInfoPage
    Replaced assets for resume thumbnails

--- dayangsurga-macro/Features/AskExpert/ExpertInfoPage/ExpertInfoViewController.swift
+++ dayangsurga-macro/Features/AskExpert/ExpertInfoPage/ExpertInfoViewController.swift
@@ -7,11 +7,23 @@
 
 import UIKit
 
-class ExpertInfoViewController: MVVMViewController<ExpertInfoViewModel> {
+class ExpertInfoViewController: MVVMViewController<ExpertInfoViewModel> /* ,UITableViewDelegate, UITableViewDataSource*/{
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 2
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        <#code#>
+//    }
+//
 
+    @IBOutlet weak var infoPageImage: UIImageView!
+    @IBOutlet weak var infoPageInstructionTable: UITableView!
+    @IBOutlet weak var infoPageDismissButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        infoPageInstructionTable.register(UINib(nibName: "ExpertInfoCell", bundle: nil), forCellReuseIdentifier: "ExpertInfoCell")
         // Do any additional setup after loading the view.
     }
     
