fd1beac4043b7855eb9ee64c54abe26d79328c4b
Olip <oliviadwisusanti@gmail.com>
2021-11-12 08:45:47 +0700

    add preload data

--- dayangsurga-macro/Core/Mock Data/PreloadAccomplishmentData.swift
+++ dayangsurga-macro/Core/Mock Data/PreloadAccomplishmentData.swift
@@ -10,14 +10,26 @@ import UIKit
 
 // suggestion
 func preloadAccomplishSuggestion(){
-    
+    AccomplishmentSuggestionRepository.shared.createAccomplishmentSuggestion(accomId: 0,
+                                                                             accomSuggestionId: 0,
+                                                                             title: "Only include certificates or awards that are relevant to the job you're applying for.",
+                                                                             desc: "")
 }
 // placeholder
 func preloadAccomplishPh(){
-    
+    AccomplishmentPlaceholderRepository.shared.createAccomplishmentPlaceholder(accomId: 0,
+                                                                               accomPlaceholderId: 0,
+                                                                               titlePlaceholder: "e.g. UI/UX & Product Design",
+                                                                               givenDatePlaceholder: "e.g. Dayang.co")
 }
 
 // accomplish dummy data
 func preloadAccomplish(){
-    
+    AccomplishmentRepository.shared.createAccomplishment(accomId: 0,
+                                                         userId: 0,
+                                                         title: "App Development Swift Certification",
+                                                         givenDate: Date(),
+                                                         issuer: "Swift.co",
+                                                         desc: "",
+                                                         isSelected: true)
 }
