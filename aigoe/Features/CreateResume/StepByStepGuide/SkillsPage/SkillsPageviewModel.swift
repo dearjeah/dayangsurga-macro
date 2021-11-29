6856b26c05972046803ff701ed78c3c8a9ac33be
Delvina J <j.delvina@gmail.com>
2021-11-29 20:02:30 +0700

    skill selection integration done

--- aigoe/Features/CreateResume/StepByStepGuide/SkillsPage/SkillsPageviewModel.swift
+++ aigoe/Features/CreateResume/StepByStepGuide/SkillsPage/SkillsPageviewModel.swift
@@ -9,8 +9,17 @@ import Foundation
 
 class SkillsPageviewModel: NSObject {
     let skillRepo = SkillRepository.shared
+    let resumeContentRepo = ResumeContentRepository.shared
     
     func getAllSkillData() -> [Skills]?{
         return skillRepo.getAllSkill()
+    }
+    
+    func addSelectedSkill(resumeId: String, skillId: String) {
+        resumeContentRepo.updateResumeContentSkill(resume_id: resumeId, newSkill_id: skillId)
+    }
+    
+    func removeUnselectedSkill(resumeId: String, skillId: String) {
+        resumeContentRepo.deleteResumeContentSkill(resume_id: resumeId, skillId: skillId)
     }
 }
