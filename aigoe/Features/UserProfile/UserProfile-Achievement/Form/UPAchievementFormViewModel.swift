5b5549434eb170ed6d994701539497ab1ccfd10f
Audrey Aurelia Chang <audreyaureliachang@gmail.com>
2021-12-28 13:26:25 +0700

    Added logic for CRUD operation
    Added logic for add, update and delete data on form view model

--- aigoe/Features/UserProfile/UserProfile-Achievement/Form/UPAchievementFormViewModel.swift
+++ aigoe/Features/UserProfile/UserProfile-Achievement/Form/UPAchievementFormViewModel.swift
@@ -24,4 +24,39 @@ class UPAchievementFormViewModel: NSObject {
         return achievementRepo.getAccomplishmentById(AccomplishmentId: id)
     }
     
+    func addAchievement(title: String, givenDate: Date, endDate: Date, status: Bool, issuer: String, desc: String)-> Bool{
+        let data = achievementRepo.createAccomplishment(
+            accomId: UUID().uuidString,
+            userId: 0,
+            title: title,
+            givenDate: givenDate,
+            endDate: endDate,
+            status: status,
+            issuer: issuer,
+            desc: desc,
+            isSelected: true
+        )
+        return data
+    }
+    
+    func updateAchievement(achievementId: String, title: String,givenDate: Date, endDate: Date, status: Bool, issuer: String, desc: String) -> Bool{
+        let data = achievementRepo.updateAccomplishment(
+            accomId: achievementId,
+            userId: 0,
+            title: title,
+            givenDate: givenDate,
+            endDate: endDate,
+            status: status,
+            issuer: issuer,
+            desc: desc,
+            isSelected: true
+        )
+        return data
+    }
+    func deleteAchievementData(dataAchievement: Accomplishment?)-> Bool{
+        let data = achievementRepo.deleteAccomplishment(data: dataAchievement ?? Accomplishment())
+        
+        return data
+    }
+    
 }
