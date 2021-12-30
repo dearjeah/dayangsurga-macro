fd8eb213ac6397da12a58eaa2ec3c41a896442c9
Delvina J <j.delvina@gmail.com>
2021-12-30 13:12:41 +0700

    [del: 211230] add personal info list

---
+++ aigoe/Features/CreateResume/StepByStepGuide/PersonalInfoPage/List/PersonalInfoList.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalInfoListVC.swift
+//  aigoe
+//
+//  Created by Delvina Janice on 29/12/21.
+//
+
+import UIKit
+
+class PersonalInfoList: UIView {
+
+    /*override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }*/
+}
