340480f16c76a3c80c2057efeb5da98948e91648
Audrey Aurelia Chang <audreyaureliachang@gmail.com>
2022-01-05 17:54:10 +0700

    Added displays and logic for view model
    Added needed components for display
    Added logic in view models for CRUD operations
    Added logic for initial setup in list VC and form VC

--- aigoe/Features/UserProfile/UserProfile-Experience/Form/UPExperiecenFormVC.swift
+++ aigoe/Features/UserProfile/UserProfile-Experience/Form/UPExperiecenFormVC.swift
@@ -9,9 +9,40 @@ import UIKit
 
 class UPExperiecenFormVC: MVVMViewController<UPExperienceFormViewModel> {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var backgroundView: DesignableView!
+    @IBOutlet weak var companyNameView: LabelWithTextField!
+    @IBOutlet weak var jobTitleView: LabelWithTextField!
+    @IBOutlet weak var jobStatusView: LabelWithSwitch!
+    @IBOutlet weak var jobPeriodView: LabelWithStartEndDate!
+    @IBOutlet weak var jobSummaryView: LabelWithTextView!
+    @IBOutlet weak var addOrDeleteButton: UIButton!
+    
+    var expPh: String = ""
+    var expSuggest = String()
+    var experience: Experience?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.viewModel = UPExperienceFormViewModel()
+        setup()
+        getInitialData()
         // Do any additional setup after loading the view.
+    }
+}
+
+//MARK: Initial Setup
+extension UPExperiecenFormVC{
+    func setup(){
+        self.title = "Experience"
+        self.navigationController?.navigationBar.prefersLargeTitles = false
+    }
+}
+
+//MARK: Core Data
+extension UPExperiecenFormVC{
+    func getInitialData(){
+        
     }
 }
