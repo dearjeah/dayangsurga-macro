4c4205f3d460848f01add415ef17271e24ac364a
Delvina J <j.delvina@gmail.com>
2022-01-28 14:18:37 +0700

    [del: 220128] add button updated

--- aigoe/Features/GlobalCustomComponent/TextButton.swift
+++ aigoe/Features/GlobalCustomComponent/TextButton.swift
@@ -10,7 +10,7 @@ import UIKit
 
 extension UIButton {
     func mediumTextButton(color: UIColor){
-        self.titleLabel?.font = UIFont.systemFont(ofSize: 17.0, weight: .medium)
         self.setTitleColor(color, for: .normal)
+        self.titleLabel?.font = UIFont.systemFont(ofSize: 17.0, weight: .medium)
     }
 }
